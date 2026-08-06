39e21a5f277818f4bf7065933e9003e2383cdff4
iqbalk96 <iqbalkurniawan934@gmail.com>
2026-08-06 11:05:27 +0700

    feat(form): integrate vee-validate with shadcn-vue

--- src/features/auth/components/LoginForm.vue
+++ src/features/auth/components/LoginForm.vue
@@ -1,18 +1,32 @@
 <script setup lang="ts">
-import { ref } from "vue";
 import type { HTMLAttributes } from "vue";
+import { toTypedSchema } from "@vee-validate/zod";
+import { useForm } from "vee-validate";
 
 import { cn } from "@/lib/utils";
+
 import { Button } from "@/components/ui/button";
 import {
   Field,
   FieldDescription,
   FieldGroup,
-  FieldLabel,
   FieldSeparator,
 } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
 import { useAuthStore } from "@/shared/stores/auth.store";
+import {
+  loginSchema,
+  type LoginSchema,
+} from "@/features/auth/schemas/login.schema";
 
 const props = defineProps<{
   class?: HTMLAttributes["class"];
@@ -20,24 +34,35 @@ const props = defineProps<{
 
 const authStore = useAuthStore();
 
-const email = ref("");
-const password = ref("");
+const form = useForm<LoginSchema>({
+  validationSchema: toTypedSchema(loginSchema),
+  initialValues: {
+    email: "",
+    password: "",
+  },
+});
 
-function onSubmit() {
+const onSubmit = form.handleSubmit((values) => {
   authStore.setUser({
     id: 1,
     name: "Demo User",
-    email: email.value,
+    email: values.email,
   });
 
-  password.value = "";
-}
+  form.resetForm({
+    values: {
+      email: "",
+      password: "",
+    },
+  });
+});
+
 </script>
 
 <template>
   <form
     :class="cn('flex flex-col gap-6', props.class)"
-    @submit.prevent="onSubmit"
+    @submit="onSubmit"
   >
     <FieldGroup>
       <div class="flex flex-col items-center gap-1 text-center">
@@ -50,41 +75,53 @@ function onSubmit() {
         </p>
       </div>
 
-      <Field>
-        <FieldLabel for="email">
-          Email
-        </FieldLabel>
+      <!-- Email -->
+      <FormField
+        v-slot="{ componentField }"
+        name="email"
+      >
+        <FormItem>
+          <FormLabel>Email</FormLabel>
 
-        <Input
-          id="email"
-          v-model="email"
-          type="email"
-          placeholder="m@example.com"
-          required
-        />
-      </Field>
+          <FormControl>
+            <Input
+              type="email"
+              placeholder="m@example.com"
+              v-bind="componentField"
+            />
+          </FormControl>
 
-      <Field>
-        <div class="flex items-center">
-          <FieldLabel for="password">
-            Password
-          </FieldLabel>
+          <FormMessage />
+        </FormItem>
+      </FormField>
 
-          <a
-            href="#"
-            class="ml-auto text-sm underline-offset-4 hover:underline"
-          >
-            Forgot your password?
-          </a>
-        </div>
+      <!-- Password -->
+      <FormField
+        v-slot="{ componentField }"
+        name="password"
+      >
+        <FormItem>
+          <div class="flex items-center">
+            <FormLabel>Password</FormLabel>
 
-        <Input
-          id="password"
-          v-model="password"
-          type="password"
-          required
-        />
-      </Field>
+            <a
+              href="#"
+              class="ml-auto text-sm underline-offset-4 hover:underline"
+            >
+              Forgot your password?
+            </a>
+          </div>
+
+          <FormControl>
+            <Input
+              type="password"
+              v-bind="componentField"
+            />
+          </FormControl>
+
+          <FormMessage />
+        </FormItem>
+      </FormField>
 
       <Field>
         <Button
@@ -95,7 +132,7 @@ function onSubmit() {
         </Button>
       </Field>
 
-      <!-- Demo Pinia State -->
+      <!-- Demo Pinia -->
       <Field v-if="authStore.isAuthenticated">
         <div class="rounded-lg border bg-muted/50 p-4">
           <p class="font-medium">
@@ -115,8 +152,8 @@ function onSubmit() {
           </div>
 
           <Button
-            variant="destructive"
             class="mt-4 w-full"
+            variant="destructive"
             @click="authStore.clear()"
           >
             Logout
@@ -130,19 +167,9 @@ function onSubmit() {
 
       <Field>
         <Button
-          variant="outline"
           type="button"
+          variant="outline"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-          >
-            <path
-              d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
-              fill="currentColor"
-            />
-          </svg>
-
           Login with GitHub
         </Button>
 
---
+++ src/features/auth/schemas/login.schema.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const loginSchema = z.object({
+    email: z
+    .string()
+    .email("Email tidak valid"),
+    password: z
+        .string()
+        .min(8, "Password minimal 8 karakter"),
+});
+
+export type LoginSchema = z.infer<typeof loginSchema>;
